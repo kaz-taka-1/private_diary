49f47995485a5fd56fab15b3478a1d1812956750
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2023-03-11 17:52:21 +0900

    問い合わせ外面用のルーテイングの追加

--- diary/urls.py
+++ diary/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'diary'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
 ]
