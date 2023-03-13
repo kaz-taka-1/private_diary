08832140db2dc5104f0419d92b22fafe5a23b549
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2023-03-14 01:15:49 +0900

    メール送信処理の追加

--- diary/forms.py
+++ diary/forms.py
@@ -21,3 +21,48 @@ class InquiryForm(forms.Form):
 
         self.fields['message'].widget.attrs['class'] = 'form-control'
         self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
+import os
+
+from django import forms
+from django.core.mail import EmailMessage
+
+
+class InquiryForm(forms.Form):
+    name = forms.CharField(label='お名前', max_length=30)
+    email = forms.EmailField(label='メールアドレス')
+    title = forms.CharField(label='タイトル', max_length=30)
+    message = forms.CharField(label='メッセージ', widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
+
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをここに入力してください。'
+
+        self.fields['title'].widget.attrs['class'] = 'form-control'
+        self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください。'
+
+        self.fields['message'].widget.attrs['class'] = 'form-control'
+        self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
+
+    def send_email(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        title = self.cleaned_data['title']
+        message = self.cleaned_data['message']
+
+        subject = 'お問い合わせ {}'.format(title)
+        message = '送信者名: {0}\nメールアドレス: {1}\nメッセージ:\n{2}'.format(name, email, message)
+        from_email = os.environ.get('FROM_EMAIL')
+        to_list = [
+            os.environ.get('FROM_EMAIL')
+        ]
+        cc_list = [
+            email
+        ]
+
+        message = EmailMessage(subject=subject, body=message, from_email=from_email, to=to_list, cc=cc_list)
+        message.send()
--- diary/views.py
+++ diary/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.contrib import messages
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import InquiryForm
+
+logger = logging.getLogger(__name__)
 
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
-
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
