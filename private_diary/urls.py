aea834220ff742b720ef3f890a57ce81f4320c13
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2023-03-23 23:28:53 +0900

    ルーティングの設定

--- private_diary/urls.py
+++ private_diary/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('diary.urls')),
+    path('accounts/', include('allauth.urls'))
 ]
 
